fa225e93b28ed5c3d87aae4bf61a0c49f163f86c
omendoza1 <99977175+omendoza1@users.noreply.github.com>
2022-12-24 14:08:51 -0500

    actualizando

--- script.js
+++ script.js
@@ -1,72 +1,77 @@
-function numPrimos() {
-    let cantidad = document.getElementById("numero1").value
-    let j = 2;
-    let numerosPrimos = [];
-    setInterval(function () {
-        location.href = location.href;
-    }, 1000 * 10);
-    for (; j < cantidad; j++) {
-        if (primo(j)) {
-            numerosPrimos.push(j);
-        }
+function ejecutarAlgoritmo() {
+    // Obtener el valor del select
+    const algoritmo = document.getElementById("algoritmo-select").value;
+    // Obtener el elemento donde se mostrará el resultado
+    const resultado = document.getElementById("resultado");
+
+    // Limpiar el contenido del elemento resultado
+    resultado.innerHTML = "";
+
+
+    if (algoritmo === "algoritmo1") {
+        // Algoritmo 1: Números primos
+        const numero = parseInt(document.getElementById("numero1").value);
+        const primos = obtenerPrimos(numero);
+        console.log(numero);
+        resultado.innerHTML = `<p>Los números primos entre 1 y ${numero} son:</p>`;
+        resultado.innerHTML += `<ul><li>${primos.join("</li><li>")}</li></ul>`;
+    } else if (algoritmo === "algoritmo2") {
+        // Algoritmo 2: Sumatoria
+        const numero = document.getElementById("numero2").value;
+        const sumatoria = obtenerSumatoria(numero);
+        resultado.innerHTML = `<p>La sumatoria es: ${sumatoria}</p>`;
+    } else if (algoritmo === "algoritmo3") {
+        // Algoritmo 3: Serie de Fibonacci
+        const numero = parseInt(document.getElementById("numero3").value);
+        const fibonacci = obtenerFibonacci(numero);
+        resultado.innerHTML = `<p>Los ${numero} términos de la serie de Fibonacci son:</p>`;
+        resultado.innerHTML += `<ul><li>${fibonacci.join("</li><li>")}</li></ul>`;
     }
-    alert("La cantidad de números primos es: " + numerosPrimos.length );
-    if (cantidad > 3) {
-        alert("Los números primos entre el 1 y el " + `${cantidad} ` + "son: " + numerosPrimos);
-    } if (numerosPrimos.length == 0) {
-        alert("El número primo entre el 1 y el " + `${cantidad} ` + "es: Ninguno");
-    } if (numerosPrimos.length == 1) {
-        alert("El número primo entre el 1 y el " + `${cantidad} ` + "es: " + numerosPrimos);
-    }
-    function primo(numero) {
-        setInterval(function () {
-            location.href = location.href;
-        }, 1000 * 10);
-        for (var i = 2; i < numero; i++) {
-            if (numero % i === 0) {
-                return false;
+}
+
+// Función para obtener los números primos entre 1 y N
+function obtenerPrimos(n) {
+    const primos = [];
+    for (let i = 2; i <= n; i++) {
+        let esPrimo = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                esPrimo = false;
+                break;
             }
         }
-        return numero !== 1;
-    }
-}
-function sum() {
-    let numeroN = document.getElementById("numero2").value;
-    let vectorSum = [numeroN];
-    let totalSum = 0;
-    let i;
-    setInterval(function () {
-        location.href = location.href;
-    }, 1000 * 10);
-    for (i = 0; i < numeroN; i++) {
-        vectorSum[i] = Math.pow((i + 1), numeroN);
-        alert((i + 1) + "^" + numeroN + " = " + vectorSum[i]);
-        totalSum = totalSum + vectorSum[i];
-    }
-    alert("La sumatoria total es: " + totalSum);
-}
-function fibonacci() {
-    const numeroN = document.getElementById("numero3").value;
-    const fib = [0, 1];
-    setInterval(function () {
-        location.href = location.href;
-    }, 1000 * 10);
-    if (numeroN < 2) {
-        alert("Por favor ingrese un número mayor a 1!")
-    } else {
-        for (i = 2; i <= numeroN; i++) {
-            fib.push(fib[i - 1] + fib[i - 2]);
-            document.writeln(fib[i]);
+        if (esPrimo) {
+            primos.push(i);
         }
     }
+    return primos;
+
 }
-function* generator() {
-    let a = 1;
-    let b = 1;
-    while (true) {
-        const nextNumber = a + b;
-        a = b;
-        b = nextNumber;
-        yield nextNumber;
+// Función para obtener la sumatoria de 1^N + 2^N + 3^N + ... + N^N
+function obtenerSumatoria(n) {
+    // Validar que n sea un número entero
+    if (!Number.isInteger(n)) {
+        return 0;
     }
+
+    let sumatoria = 0;
+
+    for (let i = 1; i <= n; i++) {
+        // Calcular la potencia y sumarla a la variable sumatoria
+        sumatoria += Math.pow(i, n);
+    }
+
+    return sumatoria;
 }
+// Función para obtener los términos de la serie de Fibonacci hasta N
+function obtenerFibonacci(n) {
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+    const fibonacci = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+    }
+    return fibonacci;
+}
+
